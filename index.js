43c2b29093dca680c37a4457deb3d27581f3c794
William <manvinedi@gmail.com>
2017-11-11 11:07:59 +0800

    update

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const fontfacegen = require('fontfacegen')
 const fs = require('fs')
-
+const path = require('path')
 const inputDir = 'input/'
 const outputDir = 'output/'
-
-var fontNameArray = fs.readdirSync(inputDir)
-// console.log(inputDir, outputDir, fontNameArray)
-
-fontNameArray.forEach(function (fontName) {
-  var fontNameLength = fontName.length
-  var ext = fontName.substring(fontNameLength - 4, fontNameLength))
-  if () return
+var fontArray = fs.readdirSync(inputDir)
+fontArray.forEach(function (font) {
+  var ext = path.extname(font)
+  var fontName = path.basename(font, ext)
+  if (ext === '.otf' || ext === '.ttf') {
+    // console.log(fontName)
+    // if (!fs.existsSync(outputDir)){
+    //   fs.mkdirSync(dir);
+    // }
+    fontfacegen({
+      source: path.join(inputDir, font),
+      dest: outputDir
+    })
+  }
 })
-
-
-// fontfacegen({
-//   source: inputDir +  + '.{ttf,otf}',
-//   dest: outputDir
-// })
